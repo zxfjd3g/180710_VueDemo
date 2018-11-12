83b839484c7ddc304b548ceae5bbb48cb4ec1337
zxfjd3g <258147149@qq.com>
2018-11-12 16:49:39 +0800

    vuex

--- src/App1.vue
+++ src/App1.vue
@@ -9,31 +9,22 @@
 </template>
 
 <script>
-
-  import {mapState, mapGetters, mapActions} from 'vuex'
-
   export default {
 
-
-    computed: {
-      ...mapState(['count']),  // {count () {return this.$store.state.count}}
-      ...mapGetters(['evenOrOdd']), // {eventOrOdd () {this.$store.getters.evenOrOdd}}
+    created () {
+      console.log('store', this.$store)
     },
 
-    methods: {
-      ...mapActions(['increment', 'decrement', 'incrementIfOdd', 'incrementAsync'])
-    }
-
-    /*computed: {
+    computed: {
       evenOrOdd () {
         return this.$store.getters.evenOrOdd
       },
       count () {
         return this.$store.state.count
       }
-    },*/
+    },
 
-    /*methods: {
+    methods: {
       increment () {
         this.$store.dispatch('increment')
       },
@@ -46,20 +37,8 @@
       incrementAsync () {
         this.$store.dispatch('incrementAsync')
       },
-    }*/
+    }
   }
-
-  /*
-  ...
-  1. 打包: 将多个数据封装成对象/数组
-      function fn (...as) {as是数组}
-      fn(1, 2)
-      fn(1, 2, 3)
-  2. 解包: 将包含n个数据的对象/数组拆分出来使用
-     const arr = [1, 2]
-     const arr2 = [3,..arr, 6]
-     <MyComp {...person}/>
-   */
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
  */
 import Vue from 'vue'
 import App from './App.vue'
+import store from './vuex/store'
 
 new Vue({
   el: '#app',
@@ -10,4 +11,5 @@ new Vue({
     App
   },
   template: '<App/>',
+  store, // 配置store==> 所有组件对象都多了一个属性: $store
 })
